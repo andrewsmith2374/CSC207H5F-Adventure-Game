4d9dd0d32e3ad65595419ef8d730985bfb49d737
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2023-11-27 18:47:53 -0500

    Preparing branch for merge

--- AdventureModel/Trolls/WordleTroll/SecretWordGenerator/SecretWordGenerator.java
+++
@@ -1,8 +0,0 @@
-package AdventureModel.Trolls.WordleTroll.SecretWordGenerator;
-
-public interface SecretWordGenerator {
-    /*
-     * Generates a String of length 5
-     */
-    public String generate();
-}
