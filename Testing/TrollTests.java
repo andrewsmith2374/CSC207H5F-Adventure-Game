a3dc555a09357fe868cd242f1beabd7d0f05201d
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2023-11-27 16:21:58 -0500

    Added testing and TrollGame

---
+++ Testing/TrollTests.java
@@ -0,0 +1,45 @@
+package Testing;
+
+import AdventureModel.Troll;
+import AdventureModel.TrollFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrollTests {
+    @Test
+    void testTrollFactory() {
+        TrollFactory tf = new TrollFactory();
+
+        try {
+            Troll t = tf.createTroll("BaseTroll");
+            t.playGame();
+            t.getRequiredItems();
+        } catch (ClassNotFoundException e) { fail(); }
+    }
+
+    @Test
+    void testTrollFactoryInvalidName() {
+        TrollFactory tf = new TrollFactory();
+
+        try {
+            Troll t = tf.createTroll("foo");
+            fail();
+        } catch (ClassNotFoundException ignored) {}
+    }
+
+    @Test
+    void testTrollFactoryEmptyDirectory() {
+        TrollFactory tf = new TrollFactory();
+
+        try {
+            tf.createTroll("EmptyTroll");
+            fail();
+        } catch (ClassNotFoundException ignored) {}
+    }
+
+    @Test
+    void testTrollFactoryClassNotTroll() {
+        // TODO: Add test
+    }
+}
