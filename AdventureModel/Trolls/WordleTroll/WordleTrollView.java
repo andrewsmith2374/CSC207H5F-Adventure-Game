afbf5f85411f8be83a71a62cab7c61290504da1f
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2023-11-27 19:27:22 -0500

    Work on WordleTrollView

--- AdventureModel/Trolls/WordleTroll/WordleTrollView.java
+++ AdventureModel/Trolls/WordleTroll/WordleTrollView.java
@@ -1,25 +1,72 @@
 package AdventureModel.Trolls.WordleTroll;
 
+import UIHelpers.UIHelper;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 public class WordleTrollView {
     private WordleTroll model;
+    private UIHelper uiHelper;
     private Stage stage;
     private GridPane gridPane;
+    private Button helpButton;
     private ArrayList<TextField> inputTextField;
+    private Boolean helpToggle = false; //is help on display?
 
     public WordleTrollView(WordleTroll model) {
-        model = model;
-        stage = new Stage();
-        gridPane = new GridPane();
-        inputTextField = new ArrayList<TextField>();
+        this.model = model;
+        uiHelper = new UIHelper();
+        initUI();
+    }
 
+    private void initUI() {
+        stage = new Stage();
+        stage.setTitle("Wordle!");
+
+        gridPane = new GridPane();
+        // GridPane, anyone?
+        gridPane.setPadding(new Insets(20));
+        gridPane.setBackground(new Background(new BackgroundFill(
+                Color.valueOf("#000000"),
+                new CornerRadii(0),
+                new Insets(0)
+        )));
+
+        //Three columns, three rows for the GridPane
+        ColumnConstraints column1 = new ColumnConstraints(150);
+        ColumnConstraints column2 = new ColumnConstraints(650);
+        ColumnConstraints column3 = new ColumnConstraints(150);
+        column3.setHgrow( Priority.SOMETIMES ); //let some columns grow to take any extra space
+        column1.setHgrow( Priority.SOMETIMES );
+
+        // Row constraints
+        RowConstraints row1 = new RowConstraints();
+        RowConstraints row2 = new RowConstraints( 550 );
+        RowConstraints row3 = new RowConstraints();
+        row1.setVgrow( Priority.SOMETIMES );
+        row3.setVgrow( Priority.SOMETIMES );
+
+        gridPane.getColumnConstraints().addAll( column1 , column2 , column1 );
+        gridPane.getRowConstraints().addAll( row1 , row2 , row1 );
+
+        helpButton = new Button("Instructions");
+        helpButton.setId("Instructions");
+        uiHelper.customizeButton(helpButton, 200, 50);
+        uiHelper.makeButtonAccessible(helpButton, "Help Button", "This button gives game instructions.", "This button gives instructions on the game controls. Click it to learn how to play.");
+
+        gridPane.add( helpButton, 1, 0, 1, 1 );  // Add buttons
+
+        inputTextField = new ArrayList<TextField>();
         var scene = new Scene(gridPane,  1000, 800);
         scene.setFill(Color.BLACK);
         this.stage.setScene(scene);
