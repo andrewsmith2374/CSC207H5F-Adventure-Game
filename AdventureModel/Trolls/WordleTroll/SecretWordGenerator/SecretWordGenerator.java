c8d22730cc1dc528b93f38a1a2fd55631cd36082
Sze Hok Lee <128108378+SzeHokLee@users.noreply.github.com>
2023-12-05 13:47:50 -0500

    Update

---
+++ AdventureModel/Trolls/WordleTroll/SecretWordGenerator/SecretWordGenerator.java
@@ -0,0 +1,8 @@
+package AdventureModel.Trolls.WordleTroll.SecretWordGenerator;
+
+public interface SecretWordGenerator {
+    /*
+     * Generates a String of length 5
+     */
+    public String generate();
+}
