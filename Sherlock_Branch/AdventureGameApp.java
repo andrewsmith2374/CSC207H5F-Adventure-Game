9c2003d5aea1a7dd844abc8ea980acf9038b70fd
Sze Hok Lee <128108378+SzeHokLee@users.noreply.github.com>
2023-12-05 00:23:56 -0500

    Don't touch my branches

---
+++ Sherlock_Branch/AdventureGameApp.java
@@ -0,0 +1,29 @@
+import AdventureModel.AdventureGame;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+/**
+ * Class AdventureGameApp.
+ */
+public class AdventureGameApp extends Application {
+
+    AdventureGame model;
+    AdventureGameView view;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /*
+    * JavaFX is a Framework, and to use it we will have to
+    * respect its control flow!  To start the game, we need
+    * to call "launch" which will in turn call "start" ...
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.model = new AdventureGame("C:\\Users\\sherl\\Documents\\Programming\\Java\\CSC207\\group_83\\Sherlock_Branch\\Games\\CelestialRealms"); //change the name of the game if you want to try something bigger!
+        this.view = new AdventureGameView(model, primaryStage);
+    }
+
+}
---
+++ Sherlock_Branch/views/LoadView.java
@@ -0,0 +1,160 @@
+
+
+import AdventureModel.AdventureGame;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.*;
+
+/**
+ * Class LoadView.
+ *
+ * Loads Serialized adventure games.
+ */
+public class LoadView {
+
+    private AdventureGameView adventureGameView;
+    private Label selectGameLabel;
+    private Button selectGameButton;
+
+    private ListView<String> GameList;
+    private String filename = null;
+
+    public LoadView(AdventureGameView adventureGameView){
+
+        //note that the buttons in this view are not accessible!!
+        this.adventureGameView = adventureGameView;
+        selectGameLabel = new Label(String.format(""));
+
+        GameList = new ListView<>(); //to hold all the file names
+
+        final Stage dialog = new Stage(); //dialogue box
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(adventureGameView.stage);
+
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.setPadding(new Insets(20, 20, 20, 20));
+        dialogVbox.setStyle("-fx-background-color: #121212;");
+        selectGameLabel.setId("CurrentGame"); // DO NOT MODIFY ID
+        GameList.setId("GameList");  // DO NOT MODIFY ID
+        GameList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        getFiles(GameList); //get files for file selector
+        selectGameButton = new Button("Change Game");
+        selectGameButton.setId("ChangeGame"); // DO NOT MODIFY ID
+        AdventureGameView.makeButtonAccessible(selectGameButton, "select game", "This is the button to select a game", "Use this button to indicate a game file you would like to load.");
+
+        //on selection, do something
+        selectGameButton.setOnAction(e -> {
+            try {
+                selectGame(selectGameLabel, GameList);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        VBox selectGameBox = new VBox(10, selectGameLabel, GameList, selectGameButton);
+
+        // Default styles which can be modified
+        GameList.setPrefHeight(100);
+        selectGameLabel.setStyle("-fx-text-fill: #e8e6e3");
+        selectGameLabel.setFont(new Font(adventureGameView.fontSize));
+        selectGameButton.setStyle("-fx-background-color: #17871b; -fx-text-fill: white;");
+        selectGameButton.setPrefSize(200, 50);
+        selectGameButton.setFont(new Font(adventureGameView.fontSize));
+        selectGameButton.setWrapText(true);
+        selectGameBox.setAlignment(Pos.CENTER);
+        dialogVbox.getChildren().add(selectGameBox);
+        Scene dialogScene = new Scene(dialogVbox, 400, 400);
+        dialog.setScene(dialogScene);
+        dialog.show();
+    }
+
+    /**
+     * Get Files to display in the on screen ListView
+     * Populate the listView attribute with .ser file names
+     * Files will be located in the Games/Saved directory
+     *
+     * @param listView the ListView containing all the .ser files in the Games/Saved directory.
+     */
+    private void getFiles(ListView<String> listView) {
+        // Set up the directory
+        File dir = new File("Games/Saved");
+        // Loop through all the files in the games/saved
+        if (dir.list().length > 0) {
+            for (File ele : dir.listFiles()) {
+                // See if the files end with .ser iff that is a file
+                if (ele.isFile()) {
+                    // Add to listView if it's end with .ser
+                    if (ele.getName().endsWith(".ser")){
+                        listView.getItems().add(ele.getName());
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Select the Game
+     * Try to load a game from the Games/Saved
+     * If successful, stop any articulation and put the name of the loaded file in the selectGameLabel.
+     * If unsuccessful, stop any articulation and start an entirely new game from scratch.
+     * In this case, change the selectGameLabel to indicate a new game has been loaded.
+     *
+     * @param selectGameLabel the label to use to print errors and or successes to the user.
+     * @param GameList the ListView to populate
+     */
+    private void selectGame(Label selectGameLabel, ListView<String> GameList) throws IOException {
+        //saved games will be in the Games/Saved folder!
+        // Try to get the selected files from GameList (ListView)
+        AdventureGame loadVersion = null;
+        try {
+            String name = GameList.getSelectionModel().getSelectedItem();
+            String dir = "Games/Saved/" + name;
+            loadVersion = loadGame(dir);
+            selectGameLabel.setText(name);
+        }
+        catch (Exception e) { // Start an entire new game if error is caught
+            String name = adventureGameView.model.getDirectoryName().substring(6);
+            loadVersion = new AdventureGame(name);
+            selectGameLabel.setText("A new game has been loaded");
+        }
+        finally {
+            adventureGameView.stopArticulation();
+            adventureGameView.model = loadVersion;
+            adventureGameView.updateScene("");
+            adventureGameView.updateItems();
+        }
+    }
+
+    /**
+     * Load the Game from a file
+     *
+     * @param GameFile file to load
+     * @return loaded Tetris Model
+     */
+    public AdventureGame loadGame(String GameFile) throws IOException, ClassNotFoundException {
+        // Reading the object from a file
+        FileInputStream file = null;
+        ObjectInputStream in = null;
+        try {
+            file = new FileInputStream(GameFile);
+            in = new ObjectInputStream(file);
+            return (AdventureGame) in.readObject();
+        } finally {
+            if (in != null) {
+                in.close();
+                file.close();
+            }
+        }
+    }
+
+}
