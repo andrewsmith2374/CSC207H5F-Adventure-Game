4dfd5bf3292acdbd4b44bb2ef4c4376424f9834b
Andrew Smith <andrewb.smith@mail.utoronto.ca>
2023-12-06 04:36:43 +0000

    [DEV-4] Fixed Instructions

--- Util/UIHelpers/UIHelper.java
+++ Util/UIHelpers/UIHelper.java
@@ -1,15 +1,7 @@
-package UIHelpers;
+package Util.UIHelpers;
 
-import AdventureModel.Trolls.WordleTroll.WordleTrollView;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.AccessibleRole;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
 
 public class UIHelper {
